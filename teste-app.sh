fc025ff7bede3455f3532070dd8f9ee3cee33578
Italo <italohmpg@gmail.com>
2024-02-11 22:38:33 -0300

    finalizando

--- teste-app.sh
+++ teste-app.sh
@@ -1,1 +1,14 @@
-#teste
+#/bin/bash
+RESULT="'wget -q0- http://localhost:8090'"
+wget -q localhost:8090
+if [ $? -eq 0 ]
+then
+    echo 'ok - serviço no ar'
+elif [[$RESULT == *"Number"*]]
+then
+    echo "ok-number of visits"
+    echo $RESULT
+else
+    echo "erro! number of visits"
+    exit 1
+fi
